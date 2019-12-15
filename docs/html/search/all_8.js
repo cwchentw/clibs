696e159cd62ca76bc7efe135aab5cef2dd682b8d
Michael Chen <cwchen123@gmail.com>
2019-12-15 08:20:43 +0800

    Update the documentation

--- docs/html/search/all_8.js
+++ docs/html/search/all_8.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['string_5fallocate_18',['string_allocate',['../cstring_8h.html#a5594f2b0ce2c840820c0a28fc44b7956',1,'cstring.c']]],
-  ['string_5fallocate_5fsubstring_19',['string_allocate_substring',['../cstring_8h.html#a4f33944441c46a997af3ebd1eeed1e82',1,'cstring.c']]],
-  ['string_5fcontains_20',['string_contains',['../cstring_8h.html#ae0bb75ef00bf73fe720b76c56c98325f',1,'cstring.c']]],
-  ['string_5fis_5fequal_21',['string_is_equal',['../cstring_8h.html#a788b79461659ad8e79a9164af28d8ef2',1,'cstring.c']]],
-  ['string_5fis_5fspace_5fonly_22',['string_is_space_only',['../cstring_8h.html#a4b78c493fbd5c995fd8b1edc5067caf2',1,'cstring.c']]],
-  ['string_5fstarts_5fwith_23',['string_starts_with',['../cstring_8h.html#ad17149d107c4366f280da504df45cc10',1,'cstring.c']]],
-  ['string_5fto_5fstream_24',['string_to_stream',['../cstring_8h.html#a28b2afbc73cc0806bbec57eb1da1d6ec',1,'cstring.c']]]
+  ['string_5fallocate_18',['string_allocate',['../cstring_8h.html#ae5545ccfb4a0e724d8c51bb09dd59975',1,'cstring.c']]],
+  ['string_5fallocate_5fsubstring_19',['string_allocate_substring',['../cstring_8h.html#a57dbcc036c590f2cb98571f619700597',1,'cstring.c']]],
+  ['string_5fcontains_20',['string_contains',['../cstring_8h.html#ac5530f6f236cfec990a0e2248a3f5004',1,'cstring.c']]],
+  ['string_5fis_5fequal_21',['string_is_equal',['../cstring_8h.html#aca96c88ab0b1f5a78953da011d066cd2',1,'cstring.c']]],
+  ['string_5fis_5fspace_5fonly_22',['string_is_space_only',['../cstring_8h.html#ae7ebe8f808dbc71ded2f909b20362d3e',1,'cstring.c']]],
+  ['string_5fstarts_5fwith_23',['string_starts_with',['../cstring_8h.html#ab54f92ad286b028ebf7c7355050cefcd',1,'cstring.c']]],
+  ['string_5fto_5fstream_24',['string_to_stream',['../cstring_8h.html#a8c07069d410eb4896af51d6b6a35bc56',1,'cstring.c']]]
 ];
