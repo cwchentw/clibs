d5d0453f5173bf7348540a56c3374d15847b4ed5
Michael Chen <cwchen123@gmail.com>
2019-12-22 19:56:45 +0800

    Update clibs documentation

--- docs/html/search/all_9.js
+++ docs/html/search/all_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['true',['TRUE',['../boolean_8h.html#aa8cecfc5c5c054d2875c03e77b7be15d',1,'boolean.h']]]
+  ['todo_25',['TODO',['../md_TODO.html',1,'']]],
+  ['true_26',['TRUE',['../boolean_8h.html#aa8cecfc5c5c054d2875c03e77b7be15d',1,'boolean.h']]]
 ];
--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -5,7 +5,7 @@ var indexSectionsWithContent =
   2: "s",
   3: "b",
   4: "adefmpt",
-  5: "c"
+  5: "ct"
 };
 
 var indexSectionNames =
