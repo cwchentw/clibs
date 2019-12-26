e4711007c863985567e86c1004bb7ef9e4360a65
Michael Chen <cwchen123@gmail.com>
2019-12-26 20:19:27 +0800

    Update clibs documentation

--- docs/html/search/all_1.js
+++ docs/html/search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['bool_1',['BOOL',['../boolean_8h.html#a3e5b8192e7d9ffaf3542f1210aec18dd',1,'boolean.h']]],
-  ['boolean_2eh_2',['boolean.h',['../boolean_8h.html',1,'']]]
+  ['bool',['BOOL',['../boolean_8h.html#a3e5b8192e7d9ffaf3542f1210aec18dd',1,'boolean.h']]],
+  ['boolean_2eh',['boolean.h',['../boolean_8h.html',1,'']]]
 ];
