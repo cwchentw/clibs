19ebbb3dac54b768e4c4c0df4bad306f75d983c2
Michael Chen <cwchen.tw@outlook.com>
2020-09-20 22:29:30 +0800

    Refactor some code

--- get_sizeof_data_type.c
+++ get_sizeof_data_type.c
@@ -1,39 +1,36 @@
 #include <stdio.h>
 
-#define END_OF_LINE  "\n"
 #define SIZE_OF_CHAR  8
 #define HEADER_FILE "_sizeof_data_type.h"
-
 
 int main(void)
 {
     FILE *fp = fopen(HEADER_FILE, "w");
 
     if (!fp) {
-        fprintf(stderr, "Failed to open file to write%s", END_OF_LINE);
+        fprintf(stderr, "Failed to open file to write\n");
         return 1;
     }
 
-    fprintf(fp, "#ifndef _SIZE_OF_DATA_TYPE_H%s", END_OF_LINE);
-    fprintf(fp, "#define _SIZE_OF_DATA_TYPE_H%s", END_OF_LINE);
-    fprintf(fp, "%s", END_OF_LINE);
+    fprintf(fp, "#ifndef _SIZE_OF_DATA_TYPE_H\n");
+    fprintf(fp, "#define _SIZE_OF_DATA_TYPE_H\n");
+    fprintf(fp, "\n");
 
-    fprintf(fp, "#define _SIZEOF_CHAR  %lu%s",
-        SIZE_OF_CHAR * sizeof(char), END_OF_LINE);
-    fprintf(fp, "#define _SIZEOF_SHORT  %lu%s",
-        SIZE_OF_CHAR * sizeof(short), END_OF_LINE);
-    fprintf(fp, "#define _SIZEOF_INT  %lu%s",
-        SIZE_OF_CHAR * sizeof(int), END_OF_LINE);
-    fprintf(fp, "#define _SIZEOF_LONG  %lu%s",
-        SIZE_OF_CHAR * sizeof(long), END_OF_LINE);
+    fprintf(fp, "#define _SIZEOF_CHAR  %lu\n",
+        SIZE_OF_CHAR * sizeof(char));
+    fprintf(fp, "#define _SIZEOF_SHORT  %lu\n",
+        SIZE_OF_CHAR * sizeof(short));
+    fprintf(fp, "#define _SIZEOF_INT  %lu\n",
+        SIZE_OF_CHAR * sizeof(int));
+    fprintf(fp, "#define _SIZEOF_LONG  %lu\n",
+        SIZE_OF_CHAR * sizeof(long));
 #if __STDC_VERSION__ >= 199901L || _MSC_VER || __GNUC__ || __clang__
-    fprintf(fp, "#define _SIZEOF_LONG_LONG  %lu%s",
-        SIZE_OF_CHAR * sizeof(long long), END_OF_LINE);
+    fprintf(fp, "#define _SIZEOF_LONG_LONG  %lu\n",
+        SIZE_OF_CHAR * sizeof(long long));
 #endif
 
-    fprintf(fp, "%s", END_OF_LINE);
-    fprintf(fp, "#endif  /* _SIZE_OF_DATA_TYPE_H */%s", END_OF_LINE);
-    fprintf(fp, "%s", END_OF_LINE);
+    fprintf(fp, "\n");
+    fprintf(fp, "#endif  /* _SIZE_OF_DATA_TYPE_H */\n");
 
     fclose(fp);
 
