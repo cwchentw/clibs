cc444fe69aad676dc3951b555777df0bd77d5f18
ByteBard <4949545+cwchentw@users.noreply.github.com>
2026-04-09 11:05:44 +0800

    Update clibs_control_structure.h

--- clibs_control_structure.h
+++ clibs_control_structure.h
@@ -2,20 +2,42 @@
  *  @brief      Common control structures.
  *  @author     ByteBard
  *  @copyright  MIT
+ *
+ *  @note
+ *  This header demonstrates that language-like control structures can
+ *  technically be implemented using macros in C.
+ *
+ *  However, this pattern can easily lead to code that is difficult to
+ *  read, debug, and maintain. Because of this risk, we will NOT introduce
+ *  any additional "language block" style macros in this project.
+ *
+ *  This macro exists purely as a technical demonstration of what is
+ *  possible in C, not as a recommended pattern for general use.
+ *
+ *  @warning
+ *  Avoid using this macro in production code. Control-flow macros can
+ *  easily evolve into a macro-based DSL and make the codebase harder to
+ *  understand.
  */
+
 #ifndef CLIBS_CONTROL_STRUCTURE_H
 #define CLIBS_CONTROL_STRUCTURE_H
 
+#include <stddef.h>
+
 /** @def     TIMES(count, block)
- *  @brief   Repeat a block of code some time(s)
- *  @param   count Counter
- *  @param   block A block of code
+ *  @brief   Repeat a block of code a given number of times
+ *  @param   count Number of repetitions
+ *  @param   block Code block to execute
  */
-#define TIMES(count, block) { \
-    size_t i; \
-    for (i = 0; i < (count); ++i) { \
-        (block); \
-    } \
-}
+#define TIMES(count, block)                     \
+    do {                                        \
+        for (size_t _clibs_times_i = 0;         \
+             _clibs_times_i < (count);          \
+             ++_clibs_times_i)                  \
+        {                                       \
+            block;                              \
+        }                                       \
+    } while (0)
 
 #endif  /* CLIBS_CONTROL_STRUCTURE_H */
